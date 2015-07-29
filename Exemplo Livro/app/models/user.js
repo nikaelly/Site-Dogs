cb3cd692fcf820128a1da70259dc73efe88e5b58
Luiz Augusto <luizaugustocsouza@gmail.com>
2015-07-29 01:02:03 -0300

    testelivro/CRUD completo

---
+++ Exemplo Livro/app/models/user.js
@@ -0,0 +1,38 @@
+// Importa os pacotes
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+
+
+// Definição da estrutura da classe
+var UserSchema = new Schema({
+	name : String,
+	username : { type : String, required: true, index : { unique : true } },
+	password : { type : String, required: true, select : false } //nao aparece no select, ao menos que seja explicito
+});
+
+//Schema.pre executa funções antes de fazer alguma coisa (parametros aceitos: init, validate, save, remove)
+
+// Antes de salvar, criptografa a senha
+UserSchema.pre('save',function(next){
+	var user = this;
+	if (!user.isModified('password')) return next();
+	bcrypt.hash(user.password, null, null, function(err, hash){
+		if (err) return next(err);
+		user.password = hash;
+		next();
+	});
+});
+
+
+//Schema.methods é um conjunto de métodos definidos pelo usuário
+
+//Metodo para comparar com a senha salva no banco
+UserSchema.methods.comparePassword = function(password) {
+	var user = this;
+
+	return bcrypt.compareSync(password, user.password);
+}
+
+// permite que outro método puxe o modelo definido
+module.exports = mongoose.model('User', UserSchema);
---
+++ Exemplo Livro/server.js
@@ -0,0 +1,145 @@
+// Dando os imports
+
+var express = require('express'), // framework do node
+	app = express(),			  // facilitar a chamada
+    bodyParser = require('body-parser'), // poder usar o body das requisições POST
+	morgan = require('morgan'),    // mandar as requests pro console
+	mongoose = require('mongoose'), // conexao com o banco
+	port = process.env.Port || 8080, // setando a porta
+	User = require('./app/models/user'); // chama a classe user
+
+
+// Configurações base 
+// =================================================================
+
+// Isso faz com que ele interprete o body das requisições HTTP POST
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json())
+
+// Isso faz com que ele gerencie requisicoes CORS (cross origin requisitions)
+app.use(function(req,res,next){
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+	next();
+});
+
+// Isso faz com que as requisições recebidas vão para o console
+app.use(morgan('dev'));
+
+// Conexão com o banco : 
+mongoose.connect('mongodb://localhost:27017/testelivro')
+
+
+// ======================================================================
+// Configurações de Rota
+
+// O primeiro parametro do app/apiRouter.get é o endereço da rota a ser tratada e o segundo é uma função do que fazer
+// quando acontecer um get naquele endereço
+
+// essa pagina ta no localhost:port/
+app.get ('/', function (req, res){
+	res.send('Bem vido ao Início.')
+})
+
+// O apiRouter vai servir como um conjunto de rotas
+var apiRouter = express.Router();
+
+
+// Chamar o(s) middlewar(es) antes das rotas que eles gerenciam, porque senão a requisição vai direto para a rota
+
+// middleware que vai ser executado em todas as requisições que chegar para a API
+apiRouter.use(function(req, res, next){
+	console.log('Tem nego entrando aki.') //Cria um log
+	next(); // Faz com que ele continue executando o fluxo e não pare aqui
+});
+
+
+// rota de teste, acessada via localhost:porta/api/
+apiRouter.get('/', function(req,res){
+	res.json({message: "Bem vindo à api."})
+})
+
+
+// dentro de apiRouter.route('/x') vai gerenciar as requisições que chegam à esse endereço
+apiRouter.route('/users')
+
+	// Rota usada para criar um usuário novo, vai ser chamado quando alguem mandar um POST para localhost:porta/api/users
+	.post(function(req,res){
+	var user = new User();
+
+	// carrega os dados que veio pela requisição em um objeto do tipo User
+	user.name = req.body.name;
+	user.username = req.body.username;
+	user.password = req.body.password;
+
+	user.save(function(err){
+		if (err){ // faz tratamento de erros
+			// se houver duplicidade de indice, o mongoose retorna o erro 11000
+			if (err.code == 11000) return res.json({ success: false, message : 'Já existe usuário com o username '+ user.username +'.'});
+			else return res.send(err);
+		}
+		res.json({ success: true, message : 'Usuário criado com sucesso.'})
+	});
+
+	})
+
+	// Rota usada para retornar todos os usuários cadastrados. 
+	// vai ser chamado quando alguém mandar um get para localhost:porta/api/users
+	.get(function(req,res){
+		User.find(function(err,users){
+			if(err) res.send(err);
+			res.json(users);
+		})
+	});
+
+
+// rotas para processar usuários passando o id
+apiRouter.route('/users/:user_id')
+	
+	// vai retornar o usuário com o id que for passado no get para o endereço localhost:porta/api/users/:id
+	.get(function(req,res){
+		User.findById(req.params.user_id, function(err,user){
+			if (err) res.send(err);
+			res.json(user);
+		});
+	})
+
+	// Vai alterar o usuário com o id passado no put, de acordo com os dados passados no corpo da requisição
+	.put(function(req,res){
+		User.findById(req.params.user_id, function(err,user){ //ele procura o usuário certo
+			if (err) res.send(err);
+			
+			// se tiver os campos na requisição, ele altera o usuário encontrado;
+
+			if (req.body.name) user.name = req.body.name;
+			if (req.body.username) user.username = req.body.username;
+			if (req.body.password) user.password = req.body.password;
+
+			user.save(function(err){
+				if (err) res.send(err);
+
+				res.json({success : true, message : 'Usuário alterado com sucesso.'});
+			});
+
+		});
+	})
+
+	// Vai remover o usuário com o id passado no delete
+	.delete(function(req,res){
+		User.remove({_id : req.params.user_id},function(err,user){
+			if (err) return res.send(err);
+			res.json({success : true, message : 'Usuário removido com sucesso.'});
+		});
+	});
+	
+
+// pendura o apiRouter em localhost:porta/api
+app.use('/api', apiRouter);
+
+
+
+// ===================================================================
+// Poe o server pra rodar
+app.listen(port);
+console.log('Roles acontecem na porta '+port);
